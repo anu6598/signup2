c1f0f0039490d36008f687bf07fb22e87d192fe7
Anuradha Ananth <anuradhadatawork@gmail.com>
2025-08-08 12:31:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-def daily_summary(df):
-    """
-    Returns daily summary of signups & anomalies.
-    """
-    return (
-        df.groupby("date")
-        .agg(
-            total_signups=("ip", "count"),
-            anomalies=("is_anomaly", "sum")
-        )
-        .reset_index()
+def daily_summary(df, date_col="timestamp", ip_col="true_client_ip"):
+    df[date_col] = pd.to_datetime(df[date_col], errors="coerce", utc=True)
+    summary = (
+        df.groupby(df[date_col].dt.date)
+          .agg(
+              total_signups=(ip_col, "count"),
+              anomalies=("is_anomaly", "sum")
+          )
+          .reset_index()
+          .rename(columns={date_col: "date"})
     )
+    return summary
