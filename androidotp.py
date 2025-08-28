4e1d35d983d1bad7c7609b52546abab866c07496
Anuradha Ananth <anuradhadatawork@gmail.com>
2025-08-28 12:26:55 +0530

    Update androidotp.py

--- androidotp.py
+++ androidotp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import dailystats
+from utils import dailystats
 import re
 
 st.set_page_config(page_title="OTP Abuse Detection Dashboard", layout="wide")
