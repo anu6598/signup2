0b5599b18da80afb1fee6fb3192ae574e513ebc4
Anuradha Ananth <anuradhadatawork@gmail.com>
2025-09-01 10:26:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -234,7 +234,7 @@ st.subheader("📅 Daily Total Signups")
 df['date'] = pd.to_datetime(df['date'])  # replace 'date' with your column if named differently
 
 # Group by date and count request_path
-daily_signups = ( df.groupby(df['date'].dt.date)['request_path'] .count() .reset_index(name='total_signups')
+daily_signups =  df.groupby(df['date'].dt.date)['request_path'] .count() .reset_index(name='total_signups')
 
 
 # Display table
